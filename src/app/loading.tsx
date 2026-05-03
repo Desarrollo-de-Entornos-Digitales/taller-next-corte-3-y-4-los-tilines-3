1dbda8dbba33cce80d8e4df5ab2ff2ab10b12edb
juanpit2 <juanpablo.ceballos05@email.com>
2026-05-03 17:28:19 -0500

    chore: setup base folder structure

---
+++ src/app/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div style={{ display: 'flex', height: '100vh', alignItems: 'center', justifyContent: 'center' }}>
+      <p style={{ fontSize: '1.5rem', fontWeight: 'bold' }}>Cargando...</p>
+    </div>
+  );
+}
